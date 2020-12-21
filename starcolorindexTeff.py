62331eca0d2470b56457785fa96311168b72359c
JosephKarpinski <37123616+JosephKarpinski@users.noreply.github.com>
2020-12-21 17:05:43 -0500

    Create starcolorindexTeff.py
    Update

---
+++ starcolorindexTeff.py
@@ -0,0 +1,139 @@
+
+
+# The color index ranges are arbitary.
+# Customize to get the best fit.
+
+
+import collections
+
+teff = collections.defaultdict(lambda: "Unknown")
+
+teff["O3V"]  = 46000
+teff["O4V"]  = 43000
+
+teff["O5V"]  =     	41500
+teff["O5.5V"]  =  	40000
+teff["O6V"]  =    	39000
+teff["O6.5V"]  =  	37300
+teff["O7V"]  =    	36500
+teff["O7.5V"]  =  	35000
+teff["O8V"]  =    	34500
+teff["O8.5V"]  =  	33000
+teff["O9V"]  =    	32500
+teff["O9.5V"]  =  	32000
+teff["B0V"]  =    	31500
+teff["B0.5V"]  =  	29000
+teff["B1V"]  =    	26000
+teff["B1.5V"]  =  	24500
+teff["B2V"]  =    	20600
+teff["B2.5V"]  =  	18500
+teff["B3V"]  =    	17000
+teff["B4V"]  =    	16700
+teff["B5V"]  =    	15700
+teff["B6V"]  =    	14500
+teff["B7V"]  =    	14000
+teff["B8V"]  =    	12500
+teff["B9V"]  =    	10700
+teff["B9.5V"]  =  	10400
+teff["A0V"]  =    	9700
+teff["A1V"]  =    	9200
+teff["A2V"]  =    	8840
+teff["A3V"]  =    	8550
+teff["A4V"]  =    	8270
+teff["A5V"]  =    	8080
+teff["A6V"]  =    	8000
+teff["A7V"]  =    	7800
+teff["A8V"]  =    	7500
+teff["A9V"]  =    	7440
+teff["F0V"]  =    	7220
+teff["F1V"]  =    	7030
+teff["F2V"]  =    	6810
+teff["F3V"]  =    	6720
+teff["F4V"]  =    	6640
+teff["F5V"]  =    	6510
+teff["F6V"]  =    	6340
+teff["F7V"]  =    	6240
+teff["F8V"]  =    	6170
+teff["F9V"]  =    	6060
+teff["F9.5V"]  =  	6000
+teff["G0V"]  =    	5920
+teff["G1V"]  =    	5880
+teff["G2V"]  =    	5770
+teff["G3V"]  =    	5720
+teff["G4V"]  =    	5680
+teff["G5V"]  =    	5660
+teff["G6V"]  =    	5590
+teff["G7V"]  =    	5530
+teff["G8V"]  =    	5490
+teff["G9V"]  =    	5340
+teff["K0V"]  =    	5280
+teff["K0.5V"]  =  	5240
+teff["K1V"]  =    	5170
+teff["K1.5V"]  =  	5140
+teff["K2V"]  =    	5040
+teff["K2.5V"]  =  	4990
+teff["K3V"]  =    	4830
+teff["K3.5V"]  =  	4700
+teff["K4V"]  =    	4600
+teff["K4.5V"]  =  	4540
+teff["K5V"]  =    	4410
+teff["K5.5V"]  =  	4330
+teff["K6V"]  =    	4230
+teff["K6.5V"]  =  	4190
+teff["K7V"]  =    	4070
+teff["K8V"]  =    	4000
+teff["K9V"]  =    	3940
+teff["M0V"]  =    	3870
+teff["M0.5V"]  =  	3800
+teff["M1V"]  =    	3700
+teff["M1.5V"]  =  	3650
+teff["M2V"]  =    	3550
+teff["M2.5V"]  =  	3500
+teff["M3V"]  =    	3410
+teff["M3.5V"]  =  	3250
+teff["M4V"]  =    	3200
+teff["M4.5V"]  =  	3100
+teff["M5V"]  =    	3030
+teff["M5.5V"]  =  	3000
+teff["M6V"]  =    	2850
+teff["M6.5V"]  =  	2710
+teff["M7V"]  =    	2650
+teff["M7.5V"]  =  	2600
+teff["M8V"]  =    	2500
+teff["M8.5V"]  =  	2440
+teff["M9V"]  =    	2400
+teff["M9.5V"]  =  	2320
+teff["L0V"]  =    	2250
+teff["L1V"]  =    	2100
+teff["L2V"]  =    	1960
+teff["L3V"]  =    	1830
+teff["L4V"]  =    	1700
+teff["L5V"]  =    	1590
+teff["L6V"]  =    	1490
+teff["L7V"]  =    	1410
+teff["L8V"]  =    	1350
+teff["L9V"]  =    	1300
+teff["T0V"]  =    	1260
+teff["T1V"]  =    	1230
+teff["T2V"]  =    	1200
+teff["T3V"]  =    	1160
+teff["T4V"]  =    	1120
+teff["T4.5V"]  =  	1090
+teff["T5V"]  =    	1050
+teff["T5.5V"]  =  	1010
+teff["T6V"]  =    	960
+teff["T7V"]  =    	840
+teff["T7.5V"]  =  	770
+teff["T8V"]  =    	700
+teff["T8.5V"]  =  	610
+teff["T9V"]  =    	530
+teff["T9.5V"]  =  	475
+teff["Y0V"]  =    	420
+teff["Y0.5V"]  =  	390
+teff["Y1V"]  =    	350
+teff["Y1.5V"]  =  	325
+teff["Y2V"]  =    	250
+
+
+
+
